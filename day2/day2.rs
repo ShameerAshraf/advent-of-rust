93996cc1b83453c024b120cbcce726e4f629cc02
ShameerAshraf <14112771+ShameerAshraf@users.noreply.github.com>
2024-12-28 15:45:38 -0500

    alt day2

--- day2/day2.rs
+++ day2/day2.rs
@@ -1,11 +1,11 @@
 pub fn main() {
     let gift_message = String::from("Merry Christmas! Enjoy your gift!");
-    attach_message_to_present(&gift_message);
+    attach_message_to_present(&gift_message[0..gift_message.len()]);
 
     println!("{}", gift_message);
 }
 
-pub fn attach_message_to_present(message: &String) {
+pub fn attach_message_to_present(message: &str) {
     println!("The present now has this message: {}", message);
 }
 
